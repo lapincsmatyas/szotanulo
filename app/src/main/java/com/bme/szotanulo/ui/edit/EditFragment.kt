dda168489cfe256994621240ff49dc225f34f204
lapincs.matyas <lapincs.matyas@gmail.com>
2022-04-27 14:44:17 +0200

    navigation added

--- app/src/main/java/com/bme/szotanulo/ui/edit/EditFragment.kt
+++ app/src/main/java/com/bme/szotanulo/ui/edit/EditFragment.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.bme.szotanulo.R
 import com.bme.szotanulo.databinding.EditFragmentBinding
+import com.bme.szotanulo.databinding.MainFragmentBinding
+import com.bme.szotanulo.ui.main.MainFragmentDirections
+import com.bme.szotanulo.ui.main.MainViewModel
 
 class EditFragment : Fragment() {
-    private var _binding: EditFragmentBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: EditFragmentBinding
 
     private lateinit var viewModel: EditViewModel
 
@@ -20,15 +24,21 @@ class EditFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.edit_fragment,
+            container,
+            false
+        )
         viewModel = ViewModelProvider(this)[EditViewModel::class.java];
 
-        _binding = EditFragmentBinding.inflate(inflater, container, false)
+        binding.mainButton.setOnClickListener{onMainButton()}
+
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        _binding = null
+    private fun onMainButton(){
+        val action = EditFragmentDirections.actionEditFragmentToMainFragment();
+        findNavController().navigate(action)
     }
 }
